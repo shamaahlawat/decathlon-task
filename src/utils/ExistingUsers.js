0aeec120ebcceec5b033d00152708ba21ab93879
Shama Ahlawat <shamaahlawat@gmail.com>
2021-04-07 11:26:46 +0530

    Update ExistingUsers.js

--- src/utils/ExistingUsers.js
+++ src/utils/ExistingUsers.js
@@ -1,9 +1,5 @@
 export const users_list = [
     {
-      email:'shama_ahlawat@mckinsey.com',
-      password:'admin@123'
-    },
-    {
       email:'shamaahlawat@gmail.com',
       password:'admin@123'
     },
